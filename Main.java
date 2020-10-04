58e8334958df8bd492f9b6198f1dcc6bc755cb6c
Adam Lamont <adam_lamont@wrdsb.ca>
2020-10-04 12:07:10 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * This program helps user identify items in the natural world
+ * @author David Toma
  */
 public class Main {
 
@@ -9,7 +10,26 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // import scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // ask user if the animal has feathers
+    System.out.println("Does the animal have feathers?");
+    String feathers = input.nextLine();
+
+    if(feathers.equals("yes")){
+      System.out.println("Does the animal swim?");
+      String swim = input.nextLine();
+      if(swim.equals("yes")){
+      System.out.println("This is a duck");
+      }
+    }else{
+      System.out.println("Does the animal have legs?");
+      String legs = input.nextLine();
+      if(legs.equals("yes")){
+        System.out.println("This is a lizard");
+      }
+    }
     
   }
 }
